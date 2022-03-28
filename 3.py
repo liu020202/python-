04f7bd5b532691dbb99c42d052540c27f0a22f2a
liu020202 <102136738+liu020202@users.noreply.github.com>
2022-03-28 21:28:53 +0800

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,26 @@
+y = int(input("請輸入西元年:"))
+y = y-1911
+if y % 12 == 0:
+    print("pig")
+elif y % 12 == 1:
+    print("rat")
+elif y % 12 == 2:
+    print("ox")
+elif y % 12 == 3:
+    print("tiger")
+elif y % 12 == 4:
+    print("rabbit")
+elif y % 12 == 5:
+    print("dragon")
+elif y % 12 == 6:
+    print("snake")
+elif y % 12 == 7:
+    print("horse")
+elif y % 12 == 8:
+    print("sheep")
+elif y % 12 == 9:
+    print("monkey")
+elif y % 12 == 10:
+    print("rooster")
+elif y % 12 == 11:
+    print("dog")
